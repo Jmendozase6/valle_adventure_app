08ecc4bd84906dfcf41854253f54f8f2f2dba0a7
Jmendozase6 <jmendozase6@ucvvirtual.edu.pe>
2024-05-03 01:38:30 -0500

    add: riverpod init

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,17 @@ import 'package:flutter/material.dart';
 
 // Localization
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // Responsive
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -23,6 +27,32 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         localizationsDelegates: AppLocalizations.localizationsDelegates,
         supportedLocales: AppLocalizations.supportedLocales,
+        home: TestScreen(),
+      ),
+    );
+  }
+}
+
+class TestScreen extends StatelessWidget {
+  const TestScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(AppLocalizations.of(context)!.hello),
+            Container(
+              width: 1.sw,
+              height: 100.h,
+              color: Colors.red,
+            )
+          ],
+        ),
       ),
     );
   }
