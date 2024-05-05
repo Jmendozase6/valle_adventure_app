837cf6d6af813fffffff83094c8423cf9b72af7a
Jmendozase6 <jmendozase6@ucvvirtual.edu.pe>
2024-05-05 17:35:00 -0500

    add: colors to AppColors

--- lib/core/config/theme/app_colors.dart
+++ lib/core/config/theme/app_colors.dart
@@ -2,6 +2,14 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const mainColor = Color(0xFF00A801);
+  static const mainColorDark = Color(0XFF022B00);
   static const darkColor = Color(0xFF1B1B1B);
   static const lightColor = Color(0xFFF1F3F4);
+  static const redColor = Color(0xFFEF233C);
+  static const yellowColor = Color(0xFFFFB703);
+
+  static Color darkColor20 = const Color(0xFF1B1B1B).withOpacity(0.20);
+
+  static Color whiteColor = Colors.white;
+  static Color blackColor = Colors.black;
 }
