31ad33309756f84b66bf19404711687363865b21
Jhair <jmendozase6@ucvvirtual.edu.pe>
2024-10-20 00:51:37 -0500

    refactor: Add unit tests for onboardingProvider

---
+++ test/features/onboarding/presentation/providers/onboarding_provider_test.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:valle_adventure_app/features/onboarding/presentation/providers/onboarding_provider.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  group('onboardingProvider', () {
+    test('returns false when hasOnboarded is not set', () async {
+      SharedPreferences.setMockInitialValues({});
+
+      final container = ProviderContainer();
+      final result = await container.read(onboardingProvider.future);
+
+      expect(result, false);
+    });
+
+    test('returns true when hasOnboarded is set to true', () async {
+      SharedPreferences.setMockInitialValues({'hasOnboarded': true});
+
+      final container = ProviderContainer();
+      final result = await container.read(onboardingProvider.future);
+
+      expect(result, true);
+    });
+
+    test('returns false when hasOnboarded is set to false', () async {
+      SharedPreferences.setMockInitialValues({'hasOnboarded': false});
+
+      final container = ProviderContainer();
+      final result = await container.read(onboardingProvider.future);
+
+      expect(result, false);
+    });
+  });
+}
